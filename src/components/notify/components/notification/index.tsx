a206fde152f620b1d8c2352c1c1fffcf84c0c3a7
min <min@ibears.io>
2022-10-26 19:59:56 +0800

    add notification component

---
+++ src/components/notify/components/notification/index.tsx
@@ -0,0 +1,36 @@
+import cx from 'classnames';
+import classes from './style.module.scss';
+import { NotifyGeneratorProps } from '../../notification-generator';
+
+interface AppProps extends NotifyGeneratorProps {
+	order: number;
+}
+
+const Notification = ({
+	text,
+	duration,
+	icon: Icon,
+	className,
+	order,
+}: AppProps) => {
+	const _getAnimationDuration = (duration: number) => `${duration / 1000}s`;
+
+	const _getTop = (order: number) => 20 + 100 * order;
+
+	return (
+		<div
+			className={classes.notification}
+			style={{
+				animationDuration: _getAnimationDuration(duration),
+				top: _getTop(order),
+			}}
+		>
+			<p>{text}</p>
+			<i className={cx('icon', className)}>
+				<Icon />
+			</i>
+		</div>
+	);
+};
+
+export default Notification;
