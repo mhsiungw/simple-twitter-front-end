4c47f2472008921f4b8de375c5b0fe8ee436d7c0
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-11-03 23:38:46 +0800

    show the fake photo successfully

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -2,6 +2,8 @@ import Header from "components/header"
 import HeaderLeftButton from "components/header/HeaderLeftButton"
 import { useRef, useEffect } from "react"
 import classes from "./style.module.scss"
+import Image from "next/image"
+import fakePhoto from "src/assets/img/fake-photo.png"
 
 function Modal({
   isOpened,
@@ -26,6 +28,16 @@ function Modal({
           <HeaderLeftButton currentUtility="modal" onClick={onClose} />
         }
       />
+      <aside>
+        <div>
+          <Image src={fakePhoto} alt="current user's avatar" />
+        </div>
+        <form method="dialog">
+          <textarea placeholder="有什麼新鮮事？"></textarea>
+          {/* 會用之後做好的button component */}
+          <button>推文</button>
+        </form>
+      </aside>
     </dialog>
   )
 }
