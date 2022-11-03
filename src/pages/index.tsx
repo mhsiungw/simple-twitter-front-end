420d32c73d30f7a0784c7fda8fd708aeeea8968f
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-11-03 15:57:01 +0800

    delete redundant component in pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,11 @@
 import type { NextPage } from "next"
 import Example from "components/example"
-import Header from "components/header"
-import HeaderLeftButton from "components/header/HeaderLeftButton"
-import HeaderTitle from "components/header/HeaderTitle"
 
 const Home: NextPage = () => {
   return (
     <>
       <div>Home</div>
       <Example message="example string" />
-      <Header headerTitle={<HeaderTitle title={"首頁"} />} />
-      <Header
-        headerLeftButton={<HeaderLeftButton />}
-        headerTitle={<HeaderTitle title={"推文"} />}
-      />
-      <Header
-        headerLeftButton={<HeaderLeftButton />}
-        headerTitle={<HeaderTitle title={["John Doe", "25 推文"]} />}
-      />
     </>
   )
 }
