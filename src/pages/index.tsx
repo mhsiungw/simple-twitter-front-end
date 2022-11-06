6f4716913040ab7a4f3b396e2cf53b8bb2b77e6a
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-11-06 12:07:40 +0800

    clean code to default

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,11 @@
 import type { NextPage } from "next"
 import Example from "components/example"
-import { Modal, tweetsData, currentUser, useOpenModal } from "components/modal"
-
-const fakeCurrentUser: currentUser = {
-  avatar: ""
-}
-
-const fakeReplyingTweets: tweetsData = {
-  tweetsContent:
-    "Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum. ",
-  tweetsOwner: {
-    name: "Apple",
-    account: "apple",
-    avatar: ""
-  },
-  tweetsCreatedAt: 2000000000000
-}
 
 const Home: NextPage = () => {
-  const [isOpened, openModal, closeModal] = useOpenModal()
   return (
     <>
-      <div onClick={openModal}>Home</div>
+      <div>Home</div>
       <Example message="example string" />
-      <Modal
-        isOpened={isOpened}
-        onClose={closeModal}
-        currentUser={fakeCurrentUser}
-        tweetsData={fakeReplyingTweets}
-      />
     </>
   )
 }
