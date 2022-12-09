47bda33cf52e734634324b23358e89e78cc66819
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-12-09 11:52:14 +0800

    re-arrange the header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,14 @@
 import type { NextPage } from "next"
 import Example from "components/example"
 import Header from "components/header"
-import HeaderLeftButton from "components/header/header-left-button"
-import HeaderTitle from "components/header/header-title"
 
 const Home: NextPage = () => {
   return (
     <>
       <div>Home</div>
       <Example message="example string" />
-      <Header
-        headerLeftButton={<HeaderLeftButton currentUtility="main" />}
-        headerTitle={<HeaderTitle title={["John Doe", 100]} />}
-      />
+      {/* <Header utility="with-icon" title={["John Doe", 25]} /> */}
+      <Header utility="normal" title="首頁" />
     </>
   )
 }
