4d800f3abc4b55063db27d4166f03fe89e0db437
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-12-09 10:42:04 +0800

    finish basic collection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import "../sass/index.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
