ab700d4f5f047d7221f8a5dd8d3ebaa92c8b462d
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-12-12 16:34:56 +0800

    to deal with the semicolon

--- src/components/modal/modal-post/index.tsx
+++ src/components/modal/modal-post/index.tsx
@@ -1,70 +1,71 @@
-import Header from "components/header"
-import ModalTemplate, { ModalProps } from "../modal-template"
-import Image from "next/image"
-import Link from "next/link"
-import fakePhoto from "../../../assets/img/fake-photo.png"
-import classes from "../style.module.scss"
-import { useRef, useState } from "react"
-import Button from "../../button"
+import React from "react";
+import Header from "components/header";
+import ModalTemplate, { ModalProps } from "../modal-template";
+import Image from "next/image";
+import Link from "next/link";
+import fakePhoto from "../../../assets/img/fake-photo.png";
+import classes from "../style.module.scss";
+import { useRef, useState } from "react";
+import Button from "../../button";
 
 interface ModalPostProps extends ModalProps {
   currentUser: {
-    readonly avatarImg?: string
-    readonly id: string
-  }
+    readonly avatarImg?: string,
+    readonly id: string,
+  },
 }
 
 const ModalPost = ({
-  isVisible,
-  onDialogClose,
-  currentUser
+	isVisible,
+	onDialogClose,
+	currentUser
 }: ModalPostProps) => {
-  const textareaRef = useRef<HTMLTextAreaElement>(null)
-  const [shouldSubmit, setShouldSubmit] = useState(false)
-  const { id, avatarImg } = currentUser
+	const textareaRef = useRef<HTMLTextAreaElement>(null);
+	const [shouldSubmit, setShouldSubmit] = useState(false);
+	const { id, avatarImg } = currentUser;
 
-  const handleDialogClose = () => {
-    const textarea = textareaRef.current
-    const textContent = textarea?.value
-    onDialogClose()
-    if (shouldSubmit && textContent) {
-      // TODO post new tweet
-      // ! need post API
-      console.log(textarea.value)
-    }
-    setShouldSubmit(false)
-    if (!textContent) return
-    textarea.value = ""
-  }
+	const handleDialogClose = () => {
+		const textarea = textareaRef.current;
+		const textContent = textarea?.value;
+		onDialogClose();
+		if (shouldSubmit && textContent) {
+			// TODO post new tweet
+			// ! need post API
+			console.log(textarea.value);
+		}
+		setShouldSubmit(false);
+		if (!textContent) return;
+		textarea.value = "";
+	};
 
-  return (
-    <ModalTemplate isVisible={isVisible} onDialogClose={handleDialogClose}>
-      <Header handleLeftClick={onDialogClose} utility="modal" />
-      <aside className={classes.modal__post}>
-        <div className={classes.avatarImg}>
-          <Link href={`/${id}`}>
-            <a>
-              <Image src={avatarImg || fakePhoto} alt="current user's avatar" />
-            </a>
-          </Link>
-        </div>
-        <form
-          method="dialog"
-          onSubmit={() => setShouldSubmit(true)}
-          className={classes.form}>
-          <textarea
-            ref={textareaRef}
-            placeholder="有什麼新鮮事？"
-            className={classes.textarea}
-          />
-          <Button className={classes.form__btn}>
-            <span>推文</span>
-          </Button>
-        </form>
-      </aside>
-    </ModalTemplate>
-  )
-}
+	return (
+		<ModalTemplate isVisible={isVisible} onDialogClose={handleDialogClose}>
+			<Header handleLeftClick={onDialogClose} utility="modal" />
+			<aside className={classes.modal__post}>
+				<div className={classes.avatarImg}>
+					<Link href={`/${id}`}>
+						<a>
+							<Image src={avatarImg || fakePhoto} alt="current user's avatar" />
+						</a>
+					</Link>
+				</div>
+				<form
+					method="dialog"
+					onSubmit={() => setShouldSubmit(true)}
+					className={classes.form}>
+					<textarea
+						ref={textareaRef}
+						placeholder="有什麼新鮮事？"
+						className={classes.textarea}
+					/>
+					<Button className={classes.form__btn}>
+						<span>推文</span>
+					</Button>
+				</form>
+			</aside>
+		</ModalTemplate>
+	);
+};
 
-export { type ModalPostProps }
-export default ModalPost
+export { type ModalPostProps };
+export default ModalPost;
--- src/components/modal/modal-template/index.tsx
+++ src/components/modal/modal-template/index.tsx
@@ -1,36 +1,36 @@
-import { useEffect, useRef, ReactNode, useState } from "react"
-import classes from "../style.module.scss"
+import React, { useEffect, useRef, ReactNode, useState } from "react";
+import classes from "../style.module.scss";
 
 interface ModalProps {
-  children?: ReactNode
-  isVisible: boolean
-  onDialogClose: () => void
+  children?: ReactNode,
+  isVisible: boolean,
+  onDialogClose: () => void,
 }
 
 const useModal: () => [boolean, () => void, () => void] = () => {
-  const [isVisible, setIsVisible] = useState(false)
-  const closeModal: () => void = () => setIsVisible(false)
-  const openModal: () => void = () => setIsVisible(true)
-  return [isVisible, openModal, closeModal]
-}
+	const [isVisible, setIsVisible] = useState(false);
+	const closeModal: () => void = () => setIsVisible(false);
+	const openModal: () => void = () => setIsVisible(true);
+	return [isVisible, openModal, closeModal];
+};
 
 const ModalTemplate = ({ isVisible, children, onDialogClose }: ModalProps) => {
-  const dialogRef = useRef<HTMLDialogElement>(null)
-  useEffect(() => {
-    const dialog = dialogRef.current
-    if (isVisible && dialog) {
-      dialog.showModal()
-    }
-    if (!isVisible && dialog) {
-      dialog.close()
-    }
-  }, [isVisible])
-  return (
-    <dialog ref={dialogRef} className={classes.modal} onClose={onDialogClose}>
-      {children}
-    </dialog>
-  )
-}
+	const dialogRef = useRef<HTMLDialogElement>(null);
+	useEffect(() => {
+		const dialog = dialogRef.current;
+		if (isVisible && dialog) {
+			dialog.showModal();
+		}
+		if (!isVisible && dialog) {
+			dialog.close();
+		}
+	}, [isVisible]);
+	return (
+		<dialog ref={dialogRef} className={classes.modal} onClose={onDialogClose}>
+			{children}
+		</dialog>
+	);
+};
 
-export { type ModalProps, useModal }
-export default ModalTemplate
+export { type ModalProps, useModal };
+export default ModalTemplate;
