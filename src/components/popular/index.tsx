a5cc639c987d5d30ff60fcf5bd0dafc90c5a5af3
chacowang0125 <chacowang0125@gmail.com>
2022-12-16 21:22:15 +0800

    fix: popular default avatar

--- src/components/popular/index.tsx
+++ src/components/popular/index.tsx
@@ -7,29 +7,25 @@ const Popular = () => {
 		{
 			username: "chaco",
 			account: "chaco123",
-			avatar:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSuf4E7MJLLAk2uGBWT65WHsUFovPjFmQItw&usqp=CAU",
+			avatar: "",
 			isFollowed: 1,
 		},
 		{
 			username: "min",
 			account: "min456",
-			avatar:
-        "https://img.wxcha.com/m00/b9/e5/88dbd5e9fd0d0d43f80d436e2ea300b4.jpg",
+			avatar: "",
 			isFollowed: 0,
 		},
 		{
 			username: "vince",
 			account: "minHappy",
-			avatar:
-        "https://i.pinimg.com/736x/ee/b0/60/eeb060ab7a3808c02948ff18699506f1.jpg",
+			avatar: "",
 			isFollowed: 0,
 		},
 		{
 			username: "weikai",
 			account: "weikaiHappyWedding",
-			avatar:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSin_WXHiPIpwirom647UPBfm94AVGsfM0DaQ&usqp=CAU",
+			avatar: "",
 			isFollowed: 0,
 		},
 	];
--- src/components/popular/popular-item/index.tsx
+++ src/components/popular/popular-item/index.tsx
@@ -1,32 +1,36 @@
+import React from "react";
+import Image from "next/image";
 import Button from "../../button";
 import styles from "./style.module.scss";
+import defaultAvatar from "../img/Photo.png";
 
 interface PopularItem {
-  username: string;
-  account: string;
-  avatar?: string;
-  isFollowed: number;
+  username: string,
+  account: string,
+  avatar?: string ,
+  isFollowed: number,
 }
 
 function PopularItem({ username, account, avatar, isFollowed }: PopularItem) {
-  const handleFollowClick = () => {
-    // Todo: post add/remove follow api
-    console.log("!!!handleFollowClick");
-  };
-  return (
-    <li className={styles.popularItem}>
-      <img className={styles.avatar} src={avatar} alt="img" />
-      <div className={styles.itemInfo}>
-        <span>{username}</span>
-        <span className={styles.account}>@{account}</span>
-      </div>
-      <Button
-        className={isFollowed === 1 ? styles.followingBtn : styles.followBtn}
-        children={isFollowed === 1 ? "正在跟隨" : "跟隨"}
-        onClick={() => handleFollowClick()}
-      />
-    </li>
-  );
+	const handleFollowClick = () => {
+		// Todo: post add/remove follow api
+		console.log("!!!handleFollowClick");
+	};
+	return (
+		<li className={styles.popularItem}>
+			<Image className={styles.avatar} src={avatar?avatar:defaultAvatar} alt="img" />
+			<div className={styles.itemInfo}>
+				<span>{username}</span>
+				<span className={styles.account}>@{account}</span>
+			</div>
+			<Button
+				className={isFollowed === 1 ? styles.followingBtn : styles.followBtn}
+				onClick={() => handleFollowClick()}
+			>
+				{isFollowed === 1 ? <p>正在跟隨</p> : <p>跟隨</p>}
+			</Button>
+		</li>
+	);
 }
 
 export default PopularItem;
