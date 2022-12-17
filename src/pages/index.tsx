33e6e627c3d2d1dfc4f2f7ce340db0eca0cfba9f
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-12-17 18:20:25 +0800

    delete popular component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import type { NextPage } from "next";
 import Example from "components/example";
-import Popular from "components/popular";
 
 const Home: NextPage = () => {
 	return (
 		<>
 			<div>Home</div>
 			<Example message="example string" />
-			<Popular />
 		</>
 	);
 };
