de67d38f4f806ac559c7ffc46087ea30fea72f92
Beginneraboutlife116 <pythonforeye@gmail.com>
2022-12-17 18:18:23 +0800

    set ul, li reset

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import type { NextPage } from "next";
 import Example from "components/example";
+import Popular from "components/popular";
 
 const Home: NextPage = () => {
 	return (
 		<>
 			<div>Home</div>
 			<Example message="example string" />
+			<Popular />
 		</>
 	);
 };
